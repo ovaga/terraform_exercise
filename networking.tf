9a631e66e9e60098022eb8fbd2e5de5b567c2b95
jude.ovaga <ovaga24@gmail.com>
2024-11-21 22:31:51 +0100

    update for networking.tf

--- networking.tf
+++ networking.tf
@@ -90,19 +90,3 @@ resource "aws_eip" "bar" {
   domain = "vpc"
 }
 
-output "vpc_id" {
-  value = aws_vpc.my_vpc.id
-
-}
-
-output "subnet_id" {
-  value = aws_subnet.public_subnet.id
-}
-
-output "subnet_id1" {
-  value = aws_subnet.private_subnet.id
-}
-
-
-
-
