e5306b07d985dc8a552fd2e52566adb8be8d4b04
jude.ovaga <ovaga24@gmail.com>
2024-11-21 22:06:21 +0100

    variable commit

---
+++ variable.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  
+}
+
+
+
+variable "cidr" {
+}
+
+variable "pub_sub_cidr" {
+  
+}
+variable "pri_sub_cidr" {
+  
+}
+
+variable "availability_1" {
+  
+}
+
+variable "instancetype" {
+  
+}
+
+variable "ami-id" {
+  
+}
+
+variable "ec2key" {
+  
+}
