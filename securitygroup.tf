e5125201d70effc787e8d9821144d1c439ab237e
jude.ovaga <ovaga24@gmail.com>
2024-11-21 22:11:48 +0100

    securitygroup commit

---
+++ securitygroup.tf
@@ -0,0 +1,23 @@
+# Security Group for both instances (allow SSH)
+resource "aws_security_group" "secgroup" {
+  name = "secgroup"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "AllowSSH"
+  }
+}
